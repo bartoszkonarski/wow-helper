6bd2b83c367b9fa96372bdabc05230540ed5e0c4
bartoszkonarski <barto.konarski@gmail.com>
2020-10-16 03:07:05 +0200

    started developing result page from form data

--- navbarpages/views.py
+++ navbarpages/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-
+from itemfinder.models import Cloth,Leather,Mail,Plate,Trinket,Other
+from django.db import models
 # Create your views here.
 def homepage(response):
     return render(response,"home.html",{})
 
 def itemfinder(response):
+    if response.method == "POST":
+        itemtype = response.POST.get("itemtype")
+        print(itemtype)
+        types = {"Cloth": Cloth,
+               "Leather": Leather,
+               "Mail": Mail,
+               "Plate": Plate,
+               "Neck/Ring": Other,
+               "Trinket": Trinket}
+        currentModel = types.get(itemtype)
+        print(currentModel)
+        q = currentModel.objects.all()
+        for item in q:
+            print(item)
+
+
+
     return render(response,"finder.html",{})
